4b05f3fb59ad14c78db53fc3bb984c9d1abc6a76
Greg Walker <mgwalker@gmail.com>
2016-08-20 11:27:54 -0500

    Fix up linting

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 
+
 ReactDOM.render(
-  <Provider store={store}>
-  </Provider>,
+  <Provider store={store} />,
   document.getElementById('content')
 );
